c0fe9b562b604b4c7604479a3bbd7e04ed431043
PeleOlal <peleolala@gmail.com>
2022-10-18 09:44:10 -0400

    init 2

--- hr_hospital/models/patient.py
+++ hr_hospital/models/patient.py
@@ -19,31 +19,32 @@ class Patient(models.Model):
     date_de_naissance = fields.Date(
         string='Date de naissance',
         required=True)
-    sexe = fields.Selection(
-        string='Sexe',
-        selection=[('masculin', _('Masculin')),
-                   ('féminin', _('Féminin')), ('indécis', _('Indécis')),],
-        required=False, )
+#    sexe = fields.Selection(
+#        string='Sexe',
+#        selection=[('masculin', _('Masculin')),
+#                   ('féminin', _('Féminin')), ('indécis', _('Indécis'))],
+#        required=False, )
     passeport = fields.Char(
         string='Le passeport',
         required=False)
     personne_de_contact = fields.Char(
         string='Personne de contact',
         required=False)
-    age = fields.Integer(
-        string='Age',
-        compute='_compute_age_patient',
-        )
-    state = fields.Selection(
-        required=True, default='disabled', selection=[
-            ('draft', 'Draft'), ('sent', 'Sent'),
-            ('test', 'Test Mode')], )
-@api.multi
-@api.depends('age')
-def _compute_age_patient(self):
-    date1 = datetime.datetime.strptime(self.date_de_naissance, DEFAULT_SERVER_DATETIME_FORMAT)
-    date_ah=datetime.date.today()
-    self.age= (date_ah.year-date1.year-1 )+(date_ah.month+12-date1.month)//12
+#    age = fields.Integer(
+#        string='Age',
+#        compute='_compute_age_patient',
+#        )
+#    state = fields.Selection(
+#        required=True, default='disabled', selection=[
+#            ('draft', 'Draft'), ('sent', 'Sent'),
+#            ('test', 'Test Mode')], )
+#@api.multi
+#@api.depends('age')
+#def _compute_age_patient(self):
+#    for card in self:
+#        date1 = datetime.datetime.strptime(card.date_de_naissance, DEFAULT_SERVER_DATETIME_FORMAT)
+#        date_ah =datetime.date.today()
+#        card.age = (date_ah.year-date1.year-1)+(date_ah.month+12-date1.month)//12
 
 
 
