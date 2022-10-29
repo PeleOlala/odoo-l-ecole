2ea67c434339fc3859489e7113cc2f3b7d74561c
PeleOlal <peleolala@gmail.com>
2022-10-29 08:24:36 -0400

    init

--- hr_hospital_3/wizard/disease_report.py
+++ hr_hospital_3/wizard/disease_report.py
@@ -1,4 +1,4 @@
-import time
+import datetime
 from odoo import fields, models, _
 
 
@@ -7,8 +7,19 @@ class DeseaseSummary(models.TransientModel):
     _name = 'hr_hospital_3.desease.summary.report'
     _description = 'Summary diseases by month'
 
-    date_from = fields.Date(string='From', required=True, default=lambda *a: time.strftime('%Y-%m-01'))
+    date_from = fields.Date(string='From')
     doctor_ids = fields.Many2many('hr_hospital_3.doctor', string='Doctor(s)')
+    year = fields.Integer(
+        string='',
+        required=False,
+        default=2022)
+    month = fields.Selection(
+        string='Month',
+        selection=[('1', 'Jan'),
+                   ('2', 'Feb'),
+                   ('3','Mar'),
+                   ('4', 'Avr')])
+
 
     def action_open_wizard(self):
         return {
@@ -23,6 +34,7 @@ class DeseaseSummary(models.TransientModel):
     def print_report(self):
         self.ensure_one()
         [data] = self.read()
+        self.date_from = datetime.date(self.year, int(self.month), 1)
         data['doctor_ids'] = self.env.context.get('active_ids', [])
         doctors = self.env['hr_hospital_3.doctor'].browse(data['doctor_ids'])
         datas = {
