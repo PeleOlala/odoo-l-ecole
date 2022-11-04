6e8656b698c0d9c3ea6b32d1001eb7cf825c7ab2
PeleOlal <peleolala@gmail.com>
2022-11-04 16:09:46 -0400

    init

--- hr_hospital_4/models/doctor.py
+++ hr_hospital_4/models/doctor.py
@@ -37,11 +37,16 @@ class DoctorExt3(models.Model):
         inverse_name='doctor_personal_id',
         string='Mes patients',
         auto_join=True)
+    intern_ids = fields.One2many(
+        comodel_name='hr_hospital_4.doctor',
+        inverse_name='mentor_id',
+        string='Mes intern',
+        auto_join=True)
 
     @api.depends('mentor_id')
     def _compute_intern(self):
         for cadr in self:
-            cadr.intern = (not cadr.mentor_id)
+            cadr.intern = (cadr.mentor_id)
 
     @api.constrains("intern")
     def _chang_intern(self):
